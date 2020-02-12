c1ac1b9eadb16ee18ec1abd804a4e4490fcfa174
Brandon Elam Barker <beb82@cornell.edu>
2020-02-12 21:32:32 +0000

    updated spectacle; removed code-slides and examples; added cornell/CAC header

--- src/Main.purs
+++ src/Main.purs
@@ -13,34 +13,38 @@ import Concur.React.Props (ReactProps)
 import Concur.React.Props as P
 import Concur.React.Run (runWidgetInDom)
 import Concur.Spectacle (codePane, deck, heading, slide)
-import Concur.Spectacle.CodeSlide (codeSlide)
-import Concur.Spectacle.CodeSlide.Props (RangeOptions, code, ranges)
 import Concur.Spectacle.Props (Progress(..), Transition(..), bgColor, lang, preload, progress, textColor, theme, transition, transitionDuration)
-import Data.Array (concat)
+import Data.Array (concat, (:))
 import Data.Time.Duration (Milliseconds(Milliseconds))
 import Effect (Effect)
-import Examples.Buttons (button2Ranges, button2Src, button2Wid, button3Ranges, button3Src, button3Wid, button4Ranges, button4Src, button4Wid, button5Ranges, button5Src, button5Wid, buttonRanges, buttonSrc, buttonWid, button6Ranges, button6Src, button6Wid, button7Ranges, button7Src, button7Wid, button8Ranges, button8Src, button8Wid)
-import Examples.Todos (todosRanges, todosSrc, todosWid)
 
 main :: Effect Unit
 main = runWidgetInDom "root" do
-    deck [ slideTheme
-         , preload assets
-         , progress Pacman
-         , transition [ Zoom, Slide ]
-         , transitionDuration (Milliseconds 1000.0)
-         ] slides
+  deck [ slideTheme
+       , preload assets
+       , progress Pacman
+       , transition [ Zoom, Slide ]
+       , transitionDuration (Milliseconds 1000.0)
+       ] slides
+{-   where
+    rootStyle = P.style{
+        "position" : "relative"
+      , "top" : "0px"
+      , "left" : "0px"
+      , "width" : "100%"
+      , "height" : "100%"
+      } -}
 
 assets :: {}
 assets = {}
 
 h :: forall a. Int -> String -> Widget HTML a
-h size txt = heading [P.style {"fontWeight": "lighter"}, P.size size, textColor white] [D.text txt]
+h size txt = heading [P.style {"fontWeight": "lighter"}, P.size size, textColor grey] [D.text txt]
 
 codeHeading :: forall a. String -> Widget HTML a
-codeHeading title = heading [ P.style { "position": "fixed"
+codeHeading title = heading [ P.style { "position": "relative"
                                       , "left": "50%"
-                                      , "top": "-120px"
+                                      , "top": "0px"
                                       , "transform": "translate(-50%)"
                                       , "padding": "20px 40px"
                                       , "border": "10px solid hotpink"
@@ -51,33 +55,38 @@ codeHeading title = heading [ P.style { "position": "fixed"
                             , P.size 1
                             ] [D.text title]
 
-codePaneSlide :: forall a. String -> Array RangeOptions -> Widget HTML a
-codePaneSlide src rangeArray =
-  codeSlide
-    [ P.style { "fontSize": "1rem" }
-    , transition []
-    , lang "haskell"
-    , code src
-    , ranges rangeArray
-    ] []
-
-codePane' :: forall a. String -> Widget HTML a
-codePane' src =
-  codePane
-    [ P.style { "fontSize": "1.4rem" }
-    , lang "haskell"
-    , code src
-    ] []
-
-
-widgetSlides :: forall a b. String -> String -> (String -> Array RangeOptions) -> Widget HTML a -> Array (Widget HTML b)
-widgetSlides title src r w =
-  [ slide [bgColor darkBg]
-      [ codeHeading title
-      , div [P.style {"zoom": "2.0"}] [disassociate w]
-      ]
-  , codePaneSlide src (r title)
+cornellLogo :: forall a. Widget HTML a
+cornellLogo = D.img [
+    P.src "images/cornell_logo_simple_white.svg"
+  , P.alt "Cornell University Logo"
+  , P.height "100px"
+  , P.style {"padding": "10px"}
   ]
+
+cacHeader :: forall a. Widget HTML a
+cacHeader = div [P.style {
+    "background-color": cornellRed
+  , "color": "white"
+  , "text-align": "left"
+  , "position": "fixed"
+  , "top": "0px"
+  , "left": "0px"
+  , "width": "100%"
+  , "display": "block"
+  }] [
+    cornellLogo
+  , D.div [P.style {
+        "position"  : "relative"
+      , "left"      : "107px"
+      , "top"       : "-40px"
+      , "font-size" : "large"
+      }]
+      [D.text "Center for Advanced Computing"]
+  ]
+
+cacSlide :: forall a. Array (Widget HTML a) -> Widget HTML a
+cacSlide es = slide [bgColor white] (cacHeader : es)
+
 
 -- Spectacle has issues with wrapping everything inside one big component
 -- So we have to create separate components for each Concur Widget
@@ -86,37 +95,25 @@ disassociate w = display [renderComponent w]
 
 slides :: forall a. Array (Widget HTML a)
 slides =
-  [ slide [bgColor red] [ h 1 "Concur", h 5 "scalable functional UIs" ]
-  , slide [bgColor blue] [ h 3 "Scalable?" ]
-  ] <> concat
-    [ widgetSlides "Hello Sailor!" buttonSrc buttonRanges buttonWid
-    , widgetSlides "Todos" todosSrc todosRanges todosWid
-    ] <>
-  [ slide [bgColor blue] [ h 3 "Functional?" ]
-  , slide [bgColor blue] [ h 3 "DSL for Functional Views" ]
-  ] <> concat
-    [ widgetSlides "Composing Widgets" button2Src button2Ranges button2Wid
-    , widgetSlides "Composing Widgets" button3Src button3Ranges button3Wid
-    ] <>
-  [ slide [bgColor blue] [ h 3 "Monadic Event Handling" ]
-  ] <> concat
-    [ widgetSlides "Event Handling" button4Src button4Ranges button4Wid
-    , widgetSlides "Nested Event Handling" button5Src button5Ranges button5Wid
-    , widgetSlides "Concurrent Event Handling" button6Src button6Ranges button6Wid
-    , widgetSlides "Handling Multiple Events" button7Src button7Ranges button7Wid
-    , widgetSlides "Elm Architecture" button8Src button8Ranges button8Wid
-    ] <>
-  [ slide [] [link "http://github.com/ajnsit/purescript-concur" "Go to Concur Purescript repository page"]
+  [ cacSlide [ h 1 "Concur", h 5 "scalable functional UIs" ]
+  , cacSlide [ h 3 "Scalable?" ]
+  ] <>
+  [ cacSlide [ h 3 "Functional?" ]
+  , cacSlide [ h 3 "DSL for Functional Views" ]
+  ] <>
+  [ cacSlide [ h 3 "Monadic Event Handling" ]
+  ] <>
+  [ cacSlide [link "http://github.com/ajnsit/purescript-concur" "Go to Concur Purescript repository page"]
   ]
 
 -- Theming
 slideTheme :: forall a. ReactProps a
 slideTheme = theme
   { colors:
-    { primary: stormy
-    , secondary: stormy
+    { primary: grey
+    , secondary: grey
     , tertiary: blue
-    , quartenary: stormy
+    , quartenary: grey
     }
   , fonts:
     { primary: "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif"
@@ -124,3 +121,10 @@ slideTheme = theme
     , tertiary: "Ahamono,Menlo,monospace"
     }
   }
+
+
+grey :: String
+grey = "#808080"
+
+cornellRed :: String
+cornellRed = "#c90022"
